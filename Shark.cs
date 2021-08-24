f736ebe9e8a1690c8e17d7effe1e543811b9bd7e
18262569939 <529324416@qq.com>
2021-08-25 04:32:39 +0800

    new upload test

--- Shark.cs
+++ Shark.cs
@@ -1,9 +1,24 @@
 using System;
-
+using System.IO;
 
 namespace Shark{
 
     public static class SharkUtils{
-        
+
+        ///<summary>
+        /// 读取本地的文件，如果文件不存在，则返回null
+        ///</summary>
+        public static string ReadFile(string filepath){
+
+            string _Ret = null;
+            if(File.Exists(filepath)){
+                FileStream file = File.OpenRead(filepath);
+                StreamReader reader = new StreamReader(file);
+                _Ret = reader.ReadToEnd();
+                reader.Close();
+                file.Close();
+            }
+            return _Ret;
+        }
     }
 }
